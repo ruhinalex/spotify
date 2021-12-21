a882a2fc89b63861d15cc384d3f9b09c48af630c
ruhinalex <ruhinalex@gmail.com>
2021-12-21 22:48:36 +0600

    first commit

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key:"PlaylistState",
+    default: null,
+})
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "37i9dQZF1DXbYM3nMM0oPk",
+})
