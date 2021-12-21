f9fdf1226b987faccd0dcff968e1999e345014e4
ruhinalex <ruhinalex@gmail.com>
2021-12-21 23:38:18 +0600

    second commit

--- components/Center.js
+++ components/Center.js
@@ -40,7 +40,7 @@ const Center = () => {
         <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={()=>{
-                    // signOut();
+                    signOut();
                 console.log("sign out has some problems");
                 }}>
                     <img className="rounded-full w-10 h-10" src={session?.user.image} alt="" />
--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,20 +5,17 @@ export async function middleware(req) {
     // Token will exist if user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-
     const { pathname } = req.nextUrl;
 
     // Redirect them to login if they don't have token AND are requesting a protected route
-    // if (!token && pathname !== '/login') {
-    //     return NextResponse.redirect('/login');
-    // }
+
 
     // Redirect them to home if they have token AND are requesting to login
-    
+
     if (token && pathname === '/login') {
         console.log(token)
         return NextResponse.redirect('/');
-    }
+    } 
 
     // Allow the requests if the following is true...
     // 1) the token exists
